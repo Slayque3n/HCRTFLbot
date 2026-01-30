5a8c8cd6076f28b3eb97c46e5431cd6b38b715cc
AkinFalase <115561726+AkinFalase@users.noreply.github.com>
2026-01-30 15:33:40 +0000

    Initial connection test
    Simple script to say hello through python, simply using pepper ip address

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-from chatwithai import *
-from maintextandspeech import *
+import qi
 
-if __name__ == "__main__":
-    language = ""
-    languages = ["ru-RU", "en-US", "de-DE", "fr-FR", "es-ES", "it-IT"] 
-    while language not in languages:
-        language = input("Enter the language code (e.g. ru-RU, en-US, de-DE, fr-FR, es-ES, it-IT): ")
+app = qi.Application()
+app.start()
+session = app.session
 
-    user_speech = speech_to_text(language)
-    
-    if user_speech:  # Check if not None or "exit"
-        llm_response = ask_llm(user_speech+" via the underground, short and simple answer, no *, answer in "+language)
-        text_to_speech(llm_response, language)
-    else:
-        print("No input received or exit command given.")
+tts = session.service("ALTextToSpeech")
+tts.say("Hello Word")
